12feadc6e1dcb2f48e87614f29d05cd6dd09036c
rushabhraj9 <40721290+rushabhraj9@users.noreply.github.com>
2020-05-10 20:05:48 +0530

    UsingMapFunction
    In this commit I have used map function in two ways.
    Refer to index.js for more info.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import emoji from "./emojipedia";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+/*var meaningstr=emoji.map(function(entries){
+  return entries.meaning.substr(0,50)
+
+});
+console.log(meaningstr);*/
+function TrimMsg(entries) {
+  return entries.meaning.substr(0, 50);
+}
+var msg = emoji.map(TrimMsg);
+console.log(msg);
